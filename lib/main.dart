c0e743fa97ca43bc63f4326164a20ff90f994aa1
Cytiez <qilla.abrar@gmail.com>
2025-11-26 12:35:09 +0700

    konekin database django

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:lume_mobile/main/screens/main_scaffold.dart'; 
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:lume_mobile/theme/lume_colors.dart';
 
+import 'package:lume_mobile/main/screens/main_scaffold.dart'; 
+
 void main() {
-  runApp(const LumeApp());
+  runApp(const MyApp());
 }
 
-class LumeApp extends StatelessWidget {
-  const LumeApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lumé',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: LumeColors.creamBackground,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: LumeColors.sageGreen,
-          primary: LumeColors.darkText,
-          secondary: LumeColors.sageGreen,
-          surface: LumeColors.creamBackground,
+    return Provider(
+      create: (_) {
+        CookieRequest request = CookieRequest();
+        return request;
+      },
+      child: MaterialApp(
+        title: 'Lumé',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: LumeColors.creamBackground,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: LumeColors.sageGreen,
+            primary: LumeColors.darkText,
+            secondary: LumeColors.sageGreen,
+            surface: LumeColors.creamBackground,
+          ),
+          useMaterial3: true,
+          textTheme: GoogleFonts.interTextTheme(
+            Theme.of(context).textTheme,
+          ),
         ),
-        useMaterial3: true,
-        textTheme: GoogleFonts.interTextTheme(
-          Theme.of(context).textTheme,
-        ),
+        // --- GANTI BAGIAN INI ---
+        // Gunakan MainScaffold agar Navbar muncul
+        home: const MainScaffold(), 
       ),
-      // Arahkan ke MainScaffold (yang ada navbarnya)
-      home: const MainScaffold(), 
     );
   }
 }
