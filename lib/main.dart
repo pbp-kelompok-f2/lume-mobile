8bed3e0bdda57f6e8e29aa323d5b96388aebb6a1
Nisrina <nisrina.fatimah@ui.ac.id>
2025-11-29 23:21:35 +0700

    benerin

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:lume_mobile/theme/lume_colors.dart';
 import 'package:lume_mobile/main/screens/main_scaffold.dart';
-import 'package:lume_mobile/cart/providers/cart_provider.dart'; // Import Provider baru
+import 'package:lume_mobile/providers/cart_provider.dart'; // Import Provider baru
 
 void main() {
   runApp(const MyApp());
